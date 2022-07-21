14a550367f4ed7eff83d6485d504dd333d8ff06a
brucetushabe <brucetushabe@gmail.com>
2022-07-21 17:25:31 +0300

    Added Application to call the Calculator

---
+++ Application.java
@@ -0,0 +1,7 @@
+public class Application{
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addition(1, 2));
+        System.out.println(calculator.addition(2.5, 4.8));
+    }
+}
